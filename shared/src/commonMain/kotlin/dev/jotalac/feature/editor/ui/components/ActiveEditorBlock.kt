e64d8c1ef32402fdf0c82c68306f9c77354a41bb
jotalac <pepa.talac@gmail.com>
2026-08-31 16:11:54 +0200

    update mobile soft keyboard enter press

--- shared/src/commonMain/kotlin/dev/jotalac/feature/editor/ui/components/ActiveEditorBlock.kt
+++ shared/src/commonMain/kotlin/dev/jotalac/feature/editor/ui/components/ActiveEditorBlock.kt
@@ -114,8 +114,10 @@ fun ActiveEditorBlock(
             editorState.updateActiveText(it, index)
         },
         onTextLayout = { textLayoutResult = it },
-        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Default),
-        keyboardActions = KeyboardActions(),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = {
+            handlePlainEnterPress()
+        }),
         modifier = modifier
             .fillMaxWidth()
             .padding(horizontal = 8.dp, vertical = 12.dp)
