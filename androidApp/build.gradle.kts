64aeec88ed3b9b2028de6ce5e98926a85d1d7ce5
jotalac <pepa.talac@gmail.com>
2026-09-05 16:55:30 +0200

    update android app version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "dev.jotalac.gitwriter"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
     }
     packaging {
         resources {
