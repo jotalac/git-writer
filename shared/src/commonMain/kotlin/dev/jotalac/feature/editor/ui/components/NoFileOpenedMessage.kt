98f6a1108c8cc396b0d2f2405ba5237370fabaf7
jotalac <pepa.talac@gmail.com>
2026-09-07 12:55:25 +0200

    add shortcut info to no file opened message

--- shared/src/commonMain/kotlin/dev/jotalac/feature/editor/ui/components/NoFileOpenedMessage.kt
+++ shared/src/commonMain/kotlin/dev/jotalac/feature/editor/ui/components/NoFileOpenedMessage.kt
@@ -1,27 +1,30 @@
 package dev.jotalac.feature.editor.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import git_writer.shared.generated.resources.Res
-import git_writer.shared.generated.resources.info_icon
-import git_writer.shared.generated.resources.no_file_opened_subtitle
-import git_writer.shared.generated.resources.no_file_opened_title
+import dev.jotalac.core.utils.isDesktopPlatform
+import git_writer.shared.generated.resources.*
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun NoFileOpenedMessage() {
+
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .focusable()
             .background(MaterialTheme.colorScheme.surface),
         contentAlignment = Alignment.Center
     ) {
@@ -39,7 +42,7 @@ fun NoFileOpenedMessage() {
             Text(
                 text = stringResource(Res.string.no_file_opened_title),
                 style = MaterialTheme.typography.titleMedium,
-                color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.4f)
+                color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.6f)
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
@@ -48,6 +51,49 @@ fun NoFileOpenedMessage() {
                 color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.3f),
                 textAlign = TextAlign.Center
             )
+
+            // display the shortcuts for desktop
+            if (isDesktopPlatform) {
+                Spacer(modifier = Modifier.height(24.dp))
+                KeyboardShortcutInfo(stringResource(Res.string.open_new_file_message), listOf("ctrl/⌘", "n"))
+            }
+        }
+    }
+}
+
+@Composable
+private fun KeyboardShortcutInfo(
+    shortcutDescription: String,
+    shortcutKeys: List<String>,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        Text(
+            shortcutDescription,
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.6f),
+            modifier = Modifier.padding(end = 20.dp)
+        )
+
+        shortcutKeys.forEach { shortcutKey ->
+            Text(
+                shortcutKey,
+                style = MaterialTheme.typography.titleSmall,
+                color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.6f),
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(MaterialTheme.colorScheme.surfaceContainer)
+                    .padding(6.dp)
+            )
+
+            if (shortcutKey != shortcutKeys.last())
+                Text(
+                    "+",
+                    style = MaterialTheme.typography.titleSmall,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.6f),
+                )
         }
     }
 }
